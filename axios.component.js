b303083d4976bf9cfb65215776128e7004efaf5b
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-24 14:26:53 +0300

    Update axios.component.js

--- axios.component.js
+++ axios.component.js
@@ -6,14 +6,15 @@ const getData = async (url) => {
     const data = (await axios.get(url)).data; //here we create a new function that get's the json data
     return data;
 };
-let dd = 'ss'
+
 const addr = "https://kaisuomi-hub.github.io/data.json";
 
 
 async function main() {
     const data = await getData(addr);
     console.log(data);
-	// return data;
+    let editeddata = '<p>' + data.employees + '</p>';
+    return editeddata;
 }
 
 main();
